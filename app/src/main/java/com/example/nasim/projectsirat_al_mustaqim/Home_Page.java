5cb46a96adfd047208591aca07b663f693b60dc6
Nasim <mahnasim24@gmail.com>
2019-01-26 21:33:24 +0600

    2133

--- app/src/main/java/com/example/nasim/projectsirat_al_mustaqim/Home_Page.java
+++ app/src/main/java/com/example/nasim/projectsirat_al_mustaqim/Home_Page.java
@@ -24,4 +24,14 @@ public class Home_Page extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+
+    }
 }
